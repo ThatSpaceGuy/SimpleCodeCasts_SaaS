e41fd6fcf8dbd8b515305665a3e8fc98ab2e6cb0
Luis Matos <ReachLuis@Gmail.com>
2015-07-20 19:44:55 +0000

    Added actionmailer settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'ReachLuis@Gmail.com'
+    default to: 'Contact@TotalMercy.com'
     
     def contact_email(name, email, body)
        @name = name
