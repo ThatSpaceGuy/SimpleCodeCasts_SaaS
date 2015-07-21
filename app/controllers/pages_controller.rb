d16ee77d2b62e05d6738a690dbe160106bb4c5aa
Luis Matos <ReachLuis@Gmail.com>
2015-07-21 16:20:00 +0000

    Added Plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
     def home
+        @believer_plan = Plan.find(1)
+        @disciple_plan = Plan.find(2)
+        @apostle_plan = Plan.find(3)
     end
     
     def about
