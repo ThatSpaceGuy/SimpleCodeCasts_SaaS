9b5fd3ac307b5dcce2459fbc2737ef9b3924e14d
Luis Matos <ReachLuis@Gmail.com>
2015-07-22 07:32:37 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,16 @@
 class ProfilesController < ApplicationController
     def new
-        # form where a user can fill out their own profile.
         @user = User.find( params[:user_id] )
-        
         @profile = @user.build_profile
+    end
+    
+    def create_table
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+    end
+    
+    private
+    def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :contact_email, :mercy_team, :description)
     end
 end
